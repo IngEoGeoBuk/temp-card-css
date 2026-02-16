1c00fe1c3af7f1294d34872fe1c2b5eb978531f7
IngEoGeoBuk <you3667@naver.com>
2026-02-16 21:35:47 +0900

    fix: 표기

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import Card from "./Card/Card";
 export default function App() {
   return (
     <div>
+      <p>unique 1</p>
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="unique" />
+      <p>unique 2</p>
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp.png'} rarity="unique" />
+      <p>legendary</p>
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="legendary" />
+      <p>rare</p>
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="rare" />
+      <p>epic</p>
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="epic" />
     </div>
   )
