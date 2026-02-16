3c07c551d2a8a8c1a9c7a0e8a662cc3fdbef8683
IngEoGeoBuk <you3667@naver.com>
2026-02-16 17:40:35 +0900

    fix: mix-blend-mode screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Card from "./Card/Card";
 export default function App() {
   return (
     <div>
+      <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="unique" />
+      <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp.png'} rarity="unique" />
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="legendary" />
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="rare" />
       <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="epic" />
-      <Card imageUrl={'https://ingeogeobuk.github.io/temp-card-css/images/temp3.png'} rarity="unique" />
     </div>
   )
 }
